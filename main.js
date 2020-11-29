2b5a7496032e06583e5347673f0440caa43da12c
Francesco Luppi <francescoluppi@aol.com>
2020-11-29 20:57:49 +0100

    Adding a random number function

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
