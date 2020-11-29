074cb2114c0c4ad4db33a9b8914796a14648af22
Francesco Luppi <francescoluppi@aol.com>
2020-11-29 21:20:30 +0100

    Adding code to create random balls and redraw

--- main.js
+++ main.js
@@ -47,6 +47,36 @@ class Ball {
     }
 }
 
-let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
-testBall.draw()
-testBall.update()
+let balls = [];
+
+while (balls.length < 25) {
+    let size = random(10, 50);
+    let ball = new Ball(
+        // ball position always drawn at least one ball width
+        // away from the edge of the canvas, to avoid drawing errors
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+        size
+    );
+
+    balls.push(ball);
+}
+
+
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop()
