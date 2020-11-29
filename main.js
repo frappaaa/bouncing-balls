bd4ddddfcafd9bbe05bfaf48a8a65a44f7eb5722
Francesco Luppi <francescoluppi@aol.com>
2020-11-29 21:35:12 +0100

    Adding the function to detect the collision detection

--- main.js
+++ main.js
@@ -45,6 +45,20 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+    collisionDetect() {
+        for (let j = 0; j < balls.length; j++) {
+            if (!(this === balls[j])) {
+                const dx = this.x - balls[j].x;
+                const dy = this.y - balls[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+                }
+            }
+        }
+    }
+
 }
 
 let balls = [];
@@ -74,6 +88,7 @@ function loop() {
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect();
     }
 
     requestAnimationFrame(loop);
